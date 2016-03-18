2d07950e25564529003e0201b3cf58d26619efef
Jason Vanderhoof <jvanderhoof@gmail.com>
2016-03-18 17:04:36 -0600

    cleanup & abstraction + formatting for message

--- config.ru
+++ config.ru
@@ -4,6 +4,10 @@ require 'bundler'
 Bundler.require
 Dotenv.load
 
+Slack.configure do |config|
+  config.token = ENV['SLACK_API_TOKEN']
+end
+
 require './mondobot'
 
 run Mondobot
--- mondobot.rb
+++ mondobot.rb
@@ -1,6 +1,8 @@
 class Mondobot < Sinatra::Base
   post '/heroku' do
     log(params)
+    heroku_message(params)
+    halt 200
   end
 
   get '/testing' do
@@ -12,4 +14,27 @@ class Mondobot < Sinatra::Base
     @log ||= Logger.new(STDOUT)
     @log.error(msg)
   end
+
+  def client
+    @client ||= Slack::Web::Client.new
+    @client.auth_test
+    @client
+  end
+
+  def app_to_channel(app)
+    {
+      'the-mondobot' => 'testing-grounds'
+    }[app]
+  end
+
+  def heroku_message(msg_hsh)
+    app = msg_hsh["app"]
+    channel = "##{app_to_channel(app)}"
+    message = <<-EOF
+    <!here> *#{app}* was deployed with the following changes:
+    `#{msg_hsh['git_log']}`
+    EOF
+    client.chat_postMessage(channel: channel, text: message, as_user: true)
+  end
+
 end
